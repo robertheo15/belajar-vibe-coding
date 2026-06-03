48c9bd7665a9eacaa54fd0722a141666da0738f1
Robert Theo <robert@gli.id>
2026-06-03 16:30:54 +0700

    feat: setup Bun, Elysia, Drizzle ORM and MySQL

---
+++ drizzle/0000_cute_agent_brand.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `users` (
+	`id` serial AUTO_INCREMENT NOT NULL,
+	`name` varchar(255) NOT NULL,
+	`email` varchar(255) NOT NULL,
+	`bio` text,
+	`created_at` timestamp DEFAULT (now()),
+	CONSTRAINT `users_id` PRIMARY KEY(`id`),
+	CONSTRAINT `users_email_unique` UNIQUE(`email`)
+);
